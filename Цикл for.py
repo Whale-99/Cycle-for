8ab3e1a6fb994277eaab0f5fd9d5b737bf2f9287
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-08-10 17:55:43 +0300

    Update Цикл for.py

--- Цикл for.py
+++ Цикл for.py
@@ -1,6 +1,23 @@
-car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-cars_count = 0
-for i in car:
-    print("Я езжу на автомобиле марки ", i)
-    cars_count += 10
-print(cars_count)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+primes = []
+not_primes = []
+
+for number in numbers:
+    if number == 1:
+        continue
+
+    is_prime = True
+
+    for divisor in range(2, number):
+        if number % divisor == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+
+print("Primes:", primes)
+print("Not Primes:", not_primes)
